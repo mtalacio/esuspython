1fcf68e458078f05d63825901dc3b174fa298d71
Matheus Talacio <mtalacio@gmail.com>
2022-02-28 16:29:13 +0000

    denovo

--- main.py
+++ main.py
@@ -201,7 +201,7 @@ def Metrics():
                 print(err)
 
             speedData, batteryData = GetSpeedAndBattery()
-            speed['text'] = str(round(speedData, 2)) + " km/h"
+            speed['text'] = str(round(float(speedData), 2)) + " km/h"
             batteryData = float(batteryData)
             batteryData = batteryData / 10
             battery['value'] = batteryData
